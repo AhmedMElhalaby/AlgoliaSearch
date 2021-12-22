4c4a12ff66e0348442617cda30e8f01391160b51
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2021-12-22 10:30:14 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,4 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::resource('posts',\App\Http\Controllers\PostController::class);
+Route::resource('users',\App\Http\Controllers\UserController::class);
