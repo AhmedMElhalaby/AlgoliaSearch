23508234bc083af264aca6ee3da1c0b32a014d10
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2021-12-28 15:24:01 +0200

    .

--- routes/web.php
+++ routes/web.php
@@ -16,5 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::resource('images',\App\Http\Controllers\ImageController::class);
 Route::resource('posts',\App\Http\Controllers\PostController::class);
 Route::resource('users',\App\Http\Controllers\UserController::class);
