f1687696dea0b73e316001e7a421c2b0c3d2e2f9
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2021-12-28 15:06:42 +0200

    .

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -17,7 +17,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+    public const HOME = '/';
 
     /**
      * The controller namespace for the application.
@@ -26,7 +26,7 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string|null
      */
-    // protected $namespace = 'App\\Http\\Controllers';
+     protected $namespace = 'App\\Http\\Controllers';
 
     /**
      * Define your route model bindings, pattern filters, etc.
@@ -42,6 +42,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->middleware('api')
                 ->namespace($this->namespace)
                 ->group(base_path('routes/api.php'));
+
+            Route::prefix('dashboard')
+                ->namespace($this->namespace.'\Dashboard')
+                ->group(base_path('routes/dashboard.php'));
 
             Route::middleware('web')
                 ->namespace($this->namespace)
