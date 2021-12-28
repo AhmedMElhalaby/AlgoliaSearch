c1e719b78633ca41fa3d1bace875dd41dee9e400
Ahmed M. Elhalaby <Eng.Ahmed.Elhalaby@gmail.com>
2021-12-28 15:28:32 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('images',\App\Http\Controllers\ImageController::class);
-Route::resource('posts',\App\Http\Controllers\PostController::class);
-Route::resource('users',\App\Http\Controllers\UserController::class);
+Route::resource('images','ImageController');
+Route::resource('posts','PostController');
+Route::resource('users','UserController');
